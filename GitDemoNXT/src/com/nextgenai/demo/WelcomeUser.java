892560bc3f3694f91d7111e8032f4f6d391a336a
aadit <aadit@LAPTOP-CTUS1HUE>
2022-06-26 10:11:41 +0530

    New project

---
+++ GitDemoNXT/src/com/nextgenai/demo/WelcomeUser.java
@@ -0,0 +1,12 @@
+package com.nextgenai.demo;
+
+public class WelcomeUser {
+
+	// code added by aditya
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to git");
+
+	}
+}
